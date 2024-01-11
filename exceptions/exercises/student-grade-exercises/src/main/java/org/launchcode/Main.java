fa452b7b24ef81f8545cc9dd84e7e31feb1efc80
sapanabagde <sapana.bagde123@gmail.com>
2024-01-11 17:01:50 -0600

    Class 10- Reading and concept check java Tooling

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,6 +1,7 @@
 package org.launchcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,15 +14,43 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        for (Map.Entry<String, String> set : studentFiles.entrySet()) {
+            int grade;
+            grade = CheckFileExtension(set.getValue());
+            System.out.println(set.getKey() + " submitted file: " + set.getValue() + "\n \t points: " + grade + "\n");
+
+        }
     }
 
-    public static void Divide(int x, int y)
-    {
+    public static double Divide(int x, int y) {
         // Write code here!
+        if (y==0) {
+            try {
+                throw new ArithmeticException("Can't divide by 0");
+            } catch (ArithmeticException a) {
+                a.printStackTrace();
+            }
+        }
+        return x/y;
+
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
+
+    public static int CheckFileExtension(String fileName) {
         // Write code here!
+        int points = 0;
+        if(fileName.isEmpty() || fileName == null) {
+            points = -1;
+            try {
+                throw new ArithmeticException("file is empty or null");
+            } catch (ArithmeticException a) {
+                a.printStackTrace();
+            }
+        }
+        if(fileName.contains(".java") && fileName.length() == (fileName.lastIndexOf('a') + 1)) {
+            points += 1;
+        }
+        return points;
+
     }
 }
